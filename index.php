fc7d51c0afffb9520e4d5bc1b76ccde1c3288c63
MiguelFlores28 <supermafl18@gmail.com>
2020-12-06 00:39:27 -0600

    Branches

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <?php
     session_start();
 ?>
@@ -119,7 +115,7 @@ and open the template in the editor.
             {
         ?>
          
-        <h1 style="font-size: 35pt">Tienda xd</h1>
+        <h1 style="font-size: 35pt">Symphony</h1>
         
         <div style="position: absolute;background-color: #97F267; 
              width: 200px; left: 50px; top: 100px; font-size: 18pt;
