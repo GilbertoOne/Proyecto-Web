b610f71cd7bcb0900450bde16c88d47585e83926
MiguelFlores28 <supermafl18@gmail.com>
2020-12-10 18:32:14 -0600

    Productos no-logged con Estilos

--- Producto.php
+++ Producto.php
@@ -8,6 +8,7 @@ and open the template in the editor.
     session_start();
 ?>
 <html>
+    
     <head>
         <meta charset="UTF-8">
         <title>Producto</title>
@@ -15,6 +16,7 @@ and open the template in the editor.
     </head>
     
     <body id="cuerpo">
+        <!--Loggeado-->
         <?php
             if(@$_SESSION['autentificado']==TRUE){
         ?>
@@ -147,34 +149,46 @@ and open the template in the editor.
             {
         ?>
          
-        <h1 style="font-size: 35pt">FOROGIL</h1>
-        
-        <div style="position: absolute;background-color: #97F267; 
-             width: 200px; left: 50px; top: 100px; font-size: 18pt;
-             text-align: center;">
-            
-            <a href="index.php"><B>Inicio</B></a>
-	
+        <div id="Banner">
+            <h1>Symphony</h1>
+            <h5>Music is the answer</h5>
         </div>
         
-        <div style="position: absolute;background-color: #85A8F6; 
-             width: 200px; left: 50px; top: 150px; font-size: 18pt;
-             text-align: center;">
+        <!--Botonera-->
+        <div id="Botonera">
+            <!--Btn inicio-->
+            <div style="cursor:pointer;" onclick="location.href='index.php'" id="InicioBtn">
+                <B>Inicio</B>
+            </div>
             
-            <a href="IniciarSesión.php"><B>Iniciar Sesión</B></a> 
-	
+            <!--Btn Sesión-->
+            <div id="InicioSesionBtn" style="top:150px; cursor:pointer;" onclick="location.href='IniciarSesión.php'">
+                <B>Iniciar Sesión</B> 
+            </div>
+        
+            <!--Btn Registro-->
+            <div style=" top: 200px; cursor:pointer;" id="RegistrarseBtn" onclick="location.href='Registrarse.php'">
+                <B>Registrarse</B> 
+            </div>
+            
+            <!--Slide de busqueda-->
+            <script>
+                $(document).ready(function(){
+                    $("#TextoB").click(function(){
+                        $("#Searcher").slideDown("slow");})
+                })
+            </script>
+            
+            <div style="top: 350px" id="BuscadorProdBtn">
+                <B id="TextoB">Buscador de productos</B>
+                <form action="Buscar.php" method="post" id="Searcher">
+                    <input type="text" name="tema" size="15"> <br>
+                    <input type="submit" value="Buscar">
+                </form>
+            </div>
         </div>
         
-        <div style="position: absolute; background-color: #F685BF; 
-             width: 200px; left: 50px; top: 200px; font-size: 18pt;
-             text-align: center;">
-            
-            <a href="Registrarse.php"><B>Registrarse</B></a> 
-	
-        </div>
-        
-        <div style="position: absolute; background-color: khaki;
-             left: 300px; top: 100px; text-align: center; font-size: 18pt">
+        <div id="PanelPrincipal">
         
         <?php
         // put your code here
@@ -231,7 +245,10 @@ and open the template in the editor.
         <?php
             }
         ?>
-            
+            </div>
         </div>   
+        
+        <!--No loggeado-->
+        
     </body>
 </html>
